be1fadd8e5e5bb30508788414d34abd3b15b97f1
Brock T <brockthigpen@yahoo.com>
2019-11-23 23:17:32 -0500

    created functioning dropdown options

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,23 @@
 $(document).foundation()
 // TODO: 
-//       use lat and long to get cuisine types in city or pre defined radius
+//       
 //       use cusine type and lat/long to generate list of places
 //       display places dynamicaly in cards with small amount of info
 //       if card is clicked launch modal with more detailed info
 //       
 //       
 //       ???????????
+let userLat;
+let userLong;
+// onclick for going out to eat option
 document.getElementById('goingOutImg').addEventListener('click', () => {
     // getting user lat and long 
     navigator.geolocation.getCurrentPosition(
         // Success callback
         function (position) {
-            let userLat = position.coords.latitude;
-            let userLong = position.coords.longitude;
-            buildApiUrl(userLat, userLong);
+            userLat = position.coords.latitude;
+            userLong = position.coords.longitude;
+            // buildApiUrl(userLat, userLong);
             console.log(userLat, userLong);
             renderOptions();
         },
@@ -22,31 +25,38 @@ document.getElementById('goingOutImg').addEventListener('click', () => {
         function (error) {
             console.log(error);
             if (error.code === 1) {
-                deniedmodal();
+                document.getElementById('modalH').innerText = 'Uh oh';
+                document.getElementById('modalLead').innerText = 'We need your loaction to show you nearby restaurants. Please close this window and try again.';
+                $(Modal1).foundation('open');
             } else {
-                wentWrongModal();
+                document.getElementById('modalH').innerText = 'Uh oh';
+                document.getElementById('modalLead').innerText = 'Something went wrong. Please close this window and try again.';
+                $(Modal1).foundation('open');
             }
         }
     )
 });
-function deniedmodal() {
-    document.getElementById('modalH').innerText = 'Uh oh';
-    document.getElementById('modalLead').innerText = 'We need your loaction to show you nearby restaurants. Please close this window and try again.';
-    $(Modal1).foundation('open');
-}
-function wentWrongModal() {
-    document.getElementById('modalH').innerText = 'Uh oh';
-    document.getElementById('modalLead').innerText = 'Something went wrong. Please close this window and try again.';
-    $(Modal1).foundation('open');
-}
-
-function buildApiUrl(userLat, userLong) {
-    //building zomato api call
-    let miles = 1;
+// on click for search button 
+document.getElementById('searchBTN').addEventListener('click', () => {
+    let order;
+    let userCuisine = document.getElementById('catSelect').value;
+    let userSort = document.getElementById('sortSelect').value;
+    if(userSort === 'rating'){
+        order = 'desc';
+    }else{
+        order = 'asc';
+    }
+    console.log(userCuisine, userSort, order);
+    if(!!userCuisine && !!userSort){
+    renderPlaces(userLat, userLong, userCuisine, userSort, order);
+    }
+})
+// api call get information about restaurants to be displayed later
+function renderPlaces(userLat, userLong, userCuisine, userSort, order) {
+    let miles = 100;
     let radius = miles * 1609.34;
     console.log(radius);
-    let userCuisine = '25';
-    let queryUrl = "https://developers.zomato.com/api/v2.1/search?lat=" + userLat + '&lon=' + userLong + '&cuisines=' + userCuisine + '&radius=' + radius + '&sort=real_distance&order=asc';
+    let queryUrl = "https://developers.zomato.com/api/v2.1/search?lat=" + userLat + '&lon=' + userLong + '&cuisines=' + userCuisine + '&radius=' + radius + '&sort=' + userSort + '&order=' + order;
     $.ajax({
         url: queryUrl,
         method: "GET",
@@ -55,11 +65,12 @@ function buildApiUrl(userLat, userLong) {
     }).then(function (response) {
         console.log(response);
     })
+    document.getElementById('catOptions').classList.add('hide');
 }
-// needs to take cuisine_name(catagoery) and create a item in drop down
-// needs to take corresponding cuisine_id and store it for use in other api call
-function renderOptions(){
-    function cuisineCat(userLat, userLong){
+// api call gets all food categories in the area around current lat&long
+// creates options for the dropdown
+function renderOptions() {
+    function cuisineCat(userLat, userLong) {
         let queryUrl = "https://developers.zomato.com/api/v2.1/cuisines?lat=" + userLat + '&lon=' + userLong;
         $.ajax({
             url: queryUrl,
@@ -67,18 +78,14 @@ function renderOptions(){
             headers: { "user-key": "a0916e41597909e8faa9dff1a09e8971" },
             dataType: "json",
         }).then(function (response) {
-            console.log(response);
-            for(let i = 0; i < response.cuisines.length; i++){
-                let name = response.cuisines[i].cuisine.cuisine_name;
-                console.log(name);
+            for (let i = 0; i < response.cuisines.length; i++) {
                 document.getElementById('catSelect').add(new Option(response.cuisines[i].cuisine.cuisine_name, response.cuisines[i].cuisine.cuisine_id));
-                // let options = document.createElement('option').setAttribute(value, response.cuisines[i].cuisine.cuisine_id).text = name;
-                // document.getElementById('catSelect').appendChild(options);
             }
-            
         })
     }
     cuisineCat();
+    document.getElementById('sortSelect').add(new Option('Distance', 'real_distance'));
+    document.getElementById('sortSelect').add(new Option('Populartiy', 'rating'));
     document.getElementById('goingOutImg').classList.add('hide')
     document.getElementById('stayInImg').classList.add('hide')
     document.querySelector('h3').classList.add('hide');
