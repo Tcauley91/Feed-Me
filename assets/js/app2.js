2d6fffdb36a5d22d0633657de20aa5cb495bceda
Tcauley91 <caleytyler@gmail.com>
2019-11-24 21:01:36 -0500

    push to pull master

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -10,26 +10,29 @@ $("#test-eq").hide() + $("#food-type").show();
 
 // gather search query based on user input
 
-    $("#feedMe").on("click",function(){
-
-    let cuisine = $(".cuisineInput").val();
-    let allergy = $(".allergyInput").val();
-    let diet = $(".dietInput").val();
-
-    // // API Key
-     let apiKey = "1bc689dd8d404710b20636ab06a5be1c";
-    // // queryURL with search parameters and APPID
-     let queryURL = "https://api.spoonacular.com/recipes/search?&query=" + cuisine + "&allergy=" + allergy + "&diet=" + diet + "&apiKey=" + apiKey; 
+    $("#feedMe").on("click",function(hidequery){
+    $("#showDiv").hide();
     
-    // ajax function for retreiving information
+        let cuisine = $(".cuisineInput").val();
+        let allergy = $(".allergyInput").val();
+        let diet = $(".dietInput").val();
+
+        // // API Key
+        let apiKey = "1bc689dd8d404710b20636ab06a5be1c";
+        // // queryURL with search parameters and APPID
+        let queryURL = "https://api.spoonacular.com/recipes/search?&query=" + cuisine + "&allergy=" + allergy + "&diet=" + diet + "&apiKey=" + apiKey; 
+        
+        // ajax function for retreiving information
       $.ajax({
         url: queryURL,
         method: "GET",
         ContentType: "application/json"
-      })
+        })
       .then(function(response){
       console.log(response);
+        });
 
+        
 
     });
-    });
+
